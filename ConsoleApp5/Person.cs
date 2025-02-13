3d3f99c5ef3b1f7f5c69254238d90d29d9ee646f
Finnyaster <132470133+Finnyaster@users.noreply.github.com>
2025-02-13 11:51:35 +0700

    Add files via upload

---
+++ ConsoleApp5/Person.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleApp5.ValidDators;
+using System;
+namespace ConsoleApp5
+{
+    class Person
+    {
+        public Person(int height, double weight, DateTime BirthDay, string FirstName, string Lastname)
+        {
+
+        }
+        // Рост
+        public int height { get; private set; }
+        // Вес
+        public double weight { get; private set; }
+        // Дата
+        public DateTime BirthDay { get; }
+        // Имя
+        public string FirstName { get; private set; }
+        // Фамилия
+        public string LastName { get; private set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public bool ChangeHeight(int height)
+        {
+            bool flag = intvalidator.ValidDate(height);
+            if (flag)
+            {
+                this.height = height;
+            }
+            return flag;
+        }
+        public bool ChangeWeight(int weight)
+        {
+            bool flag = stringvalidator.ValidDate(weight);
+            if (flag)
+            {
+                this.weight = weight;
+            }
+            return flag;
+        }
+    }
+}
---
+++ ConsoleApp5/ValidDators/stringvalidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.ValidDators
+{
+    class stringvalidator
+    {
+        public static bool ValidDate(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+        }
+    }
+}
